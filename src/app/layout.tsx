19bb05d30afa0dc03967b7a0118cb1fab0afe63d
korigamik-hypr <korigamik@gmail.com>
2023-11-05 02:56:16 +0530

    revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,5 @@
-'use-client'
-
 import "@/styles/globals.css";
 import { GeistSans, GeistMono } from "geist/font";
-
-import "@rainbow-me/rainbowkit/styles.css";
-import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { WagmiConfig, configureChains, createConfig } from "wagmi";
-import { mainnet, polygon } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon],
-  [publicProvider()],
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "Adhikaar",
-  projectId: "27265ad5e157ceaa8224faffb7f4af5c",
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
 
 export const metadata = {
   title: "VoteKero",
@@ -42,9 +17,7 @@ export default function RootLayout({
       <body
         className={`bg-background-50 font-sans text-text-900 ${GeistSans.variable} ${GeistMono.variable}`}
       >
-        <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
-        </WagmiConfig>
+        {children}
       </body>
     </html>
   );
